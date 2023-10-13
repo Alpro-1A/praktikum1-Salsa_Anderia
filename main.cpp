80affa8ca4248ceeb1b4805eb10cd236c79e2e0e
salsaandr <2310631170151@student.unsika.ac.id>
2023-10-13 11:06:38 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "------------------------------------------------------------------" <<endl<<endl;
+    cout << "Nama : Salsa Anderia Putri Nabila"<<endl<<endl;
+    cout << "NPM : 2310631170151 \n\n" ;
+    cout << "Kelas : A - Informatika \n\n";
+    cout << "Cita-cita  : Programmer \n\n";
+    cout << "Motivasi Masuk Informatika: Karena hanya minat dibidang informatika \n\n";
+    cout << "------------------------------------------------------------------" <<endl<<endl;
+
+}
